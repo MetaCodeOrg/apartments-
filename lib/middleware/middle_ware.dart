0f0d88672514e1e059650de4893971b719c79ebb
Ezaddeen2020 <ezaddeenalqubati774@gmail.com>
2025-03-26 05:04:58 +0300

    Initial commit

--- lib/middleware/middle_ware.dart
+++ lib/middleware/middle_ware.dart
@@ -2,17 +2,6 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_app/shared_pereferences/shared_pref.dart';
 import 'package:get/get.dart';
 
-// class AuthMiddleware extends GetMiddleware {
-//   @override
-//   RouteSettings? redirect(String? route) {
-//     if (!SharedPrefs.getBoolean(SharedPrefs.isLogin)) {
-//       return const RouteSettings(name: '/login');
-//     }
-//     return const RouteSettings(
-//         name: '/home'); // السماح بالانتقال للصفحة المطلوبة إذا كان المستخدم مسجلاً
-//   }
-// }
-
 class AuthMiddleware extends GetMiddleware {
   @override
   RouteSettings? redirect(String? route) {
